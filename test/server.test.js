e1a8c7fb61c773c84513d8581c07fde15f7c461c
Marie Kasai <marie.kasai@live.co.uk>
2016-08-17 17:08:31 +0100

    "#1 & #41 - server.test.js: creates test to test endpoints"

--- test/server.test.js
+++ test/server.test.js
@@ -1,13 +1,34 @@
+const fs = require('fs');
 const tape = require('tape');
+
 const server = require('../server/server.js');
 
-tape('server sets up', (t) => {
+tape('server sets up', (test) => {
   const options = {
     method: 'GET',
     url: '/'
   };
   server.inject(options, (reply) => {
-    t.equal(reply.statusCode, 200, 'server.js exports a basic server');
-    server.stop(t.end());
+    test.equal(reply.statusCode, 200, 'server.js exports a basic server');
+    server.stop(test.end());
   });
 });
+
+const testingEndpoints = () => {
+  const endpoints = fs.readdirSync('templates/views');
+  const failedEndpoints = [];
+  endpoints.forEach(filename => {
+    const options = {
+      method: 'GET',
+      url: '/' + filename
+    };
+    tape('testing endpoints', (test) => {
+      server.inject(options, (reply) => {
+        test.equal(reply.statusCode, 200, 'testing the endpoint: ' + filename);
+        server.stop(test.end());
+      });
+    });
+  })
+}
+
+testingEndpoints();
