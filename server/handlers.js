7b15fa68601c16ce51f916ac9bd618e9a85fb550
Marie Kasai <marie.kasai@live.co.uk>
2016-08-17 17:06:14 +0100

    "#1 & #41 handlers: creates basic createNewPrimaryUser handler function"

--- server/handlers.js
+++ server/handlers.js
@@ -4,4 +4,8 @@ handlers.serveFile = (request, reply) => {
   reply.view(request.params.path);
 }
 
+handlers.createNewPrimaryUser = (request, reply) => {
+  reply('ok');
+}
+
 module.exports = handlers;
