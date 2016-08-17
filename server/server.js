6288ad0389a7dbbac01410f573ee876e13ab4e4c
J Murphy <jmurphy.web@gmail.com>
2016-08-17 12:03:40 +0100

    set up basic handlebars #40

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 const Hapi = require('hapi');
-
 const server = new Hapi.Server();
+
+const path = require('path')
+//plugins
+const vision = require('vision')
+const routes = require('./routes.js')
+
+const handlebars = require('handlebars')
 
 server.connection({ port: 3000 });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: (request, reply) => {
-    reply('Hello world');
-  }
-});
+server.register([vision], err => {
+
+  // TODO: look into hoek
+  // Hoek.assert(!err, err)
+  server.views({
+    engines: {
+      html: handlebars
+    },
+    relativeTo: path.resolve(__dirname, '..'),
+    path: './templates',
+    layoutPath: './templates/layout',
+    helpersPath: './templates/helpers'
+  })
+})
+
+server.route(routes);
 
 module.exports = server;
