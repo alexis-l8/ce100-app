4746ce03394eb2f0dfd734de86d1cbbda50ed144
Marie Kasai <marie.kasai@live.co.uk>
2016-08-17 18:10:07 +0100

    #1 & #41 - routes: adds Joi validation for new primary-user form fields

--- server/routes.js
+++ server/routes.js
@@ -18,7 +18,19 @@ const routes = [
   {
     method: 'POST',
     path: '/add-user',
-    handler: handlers.createNewPrimaryUser
+    handler: handlers.createNewPrimaryUser,
+    config: {
+      validate: {
+        payload: {
+          first_name: Joi.string().min(1).required(),
+          last_name: Joi.string().min(1).required(),
+          email: Joi.string().email().required(),
+          organisation_id: Joi.string().min(1).required(),
+          user_type: ["primary", "admin"],
+          submit: "Submit"
+        }
+      }
+    }
   },
   {
     method: 'GET',
