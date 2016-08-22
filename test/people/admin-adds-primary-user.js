1de0b1f2398171eac50dbba70242d1a8b6ea15f7
mk4111 <marie.kasai@live.co.uk>
2016-08-22 22:40:16 +0100

    test/people: fixes issue where tests were failing

---
+++ test/people/admin-adds-primary-user.js
@@ -0,0 +1,39 @@
+const tape = require('tape');
+const client = require('redis').createClient();
+const server = require('../../server/server.js');
+
+const mockData = require('../mock-data.js');
+
+// TODO: Route should be authed
+
+tape('set up db', t => {
+  client.RPUSH('organisations', JSON.stringify(mockData.orgsAddDB), (err, data) => {
+    console.log('setup: redis response to org added: ', data);
+    t.end();
+  });
+});
+
+tape('/add-user post adds a user and updates the linked organisation', (t) => {
+  t.plan(3);
+  const options = {
+    method: 'POST',
+    url: '/add-user',
+    payload: JSON.stringify(mockData.usersAddPayload)
+  };
+  server.inject(options, reply => {
+    t.equal(reply.statusCode, 200, 'route exists and replies 200');
+    //check user added to db
+    client.LRANGE('people', 0, -1, (error, people) => {
+      t.deepEqual(JSON.parse(people[0]), mockData.newUserAdded, 'the new user has correct fields');
+      client.LRANGE('organisations', 0, -1, (error, orgs) => {
+        t.deepEqual(JSON.parse(orgs[0]), mockData.orgPostUser, 'Primary user added to organisation');
+        t.end();
+      });
+    });
+  });
+});
+
+tape.onFinish(() => {
+  client.FLUSHDB();
+  process.exit(0);
+});
---
+++ test/people/primary-user-activates-account.js
@@ -0,0 +1,47 @@
+const tape = require('tape');
+const bcrypt = require('bcrypt');
+const client = require('redis').createClient();
+const server = require('../../server/server.js');
+
+const mockData = require('../mock-data.js');
+
+// TODO: Route should be authed
+// TODO: hash password in mock-data.js
+// TODO: test without client
+
+tape('set up usersActivate.test.js', t => {
+  t.plan(2);
+  client.RPUSH('organisations', JSON.stringify(mockData.orgPostUser), (error, response) => {
+    t.ok(response, 'adding an organisation: response from redis is ' + response);
+    client.RPUSH('people', JSON.stringify(mockData.newUserAdded), (error, response) => {
+      t.ok(response, 'adding a primary user: response from redis is ' + response);
+      t.end();
+    });
+  });
+});
+
+tape('testing endpoint: people/activate/{userID}', t => {
+  t.plan(3);
+  const options = {
+    method: 'POST',
+    url: '/people/activate/0',
+    payload: JSON.stringify(mockData.usersActivatePayload)
+  };
+  // hit endpoint with mock data
+  server.inject(options, reply => {
+    t.equal(reply.statusCode, 200, '/people/activate/0 route exists and replies 200');
+    // check user has been updated
+    client.LRANGE('people', 0, -1, (err, people) => {
+      const user = JSON.parse(people[0]);
+      const lastLoginDiff = Date.now() - user.last_login;
+      t.ok(lastLoginDiff > 0 && lastLoginDiff < 100, 'last_login time stamped');
+      t.ok(bcrypt.compareSync(mockData.usersActivatePayload.password, user.password), 'passwords match');
+      t.end();
+    });
+  });
+});
+
+tape.onFinish(() => {
+  client.FLUSHDB();
+  process.exit(0);
+});
