652567bbf830312ca7c8d1d77dab271df16c9ddd
mk4111 <marie.kasai@live.co.uk>
2016-08-22 22:36:10 +0100

    test/orgsAdd.test.js: renamed and put into test/organisations subdirectory

--- test/organisations/admin-adds-org.js
+++ test/organisations/admin-adds-org.js
@@ -1,8 +1,8 @@
 const tape = require('tape');
-const server = require('../server/server.js');
 const client = require('redis').createClient();
+const server = require('../../server/server.js');
 
-const mocks = require('./mocks.js');
+const mockData = require('../mock-data.js');
 
 // TODO: Route should be authed
 
@@ -16,22 +16,26 @@ tape('orgs/add adds a new organisation', t => {
   const options = {
     method: 'POST',
     url: '/orgs/add',
-    payload: JSON.stringify(mocks.orgsAddPayload)
+    payload: JSON.stringify(mockData.orgsAddPayload)
   };
   // hit endpoint with mock form
   server.inject(options, reply => {
     t.equal(200, reply.statusCode, 'route exists and replies 200');
     // check organisation has been added to db
     client.LRANGE('organisations', (err, orgs) => {
-      t.deepEqual(JSON.parse(orgs[0]), mocks.orgsAddDB, 'new organisation has correct fields');
+      t.deepEqual(JSON.parse(orgs[0]), mockData.orgsAddDB, 'new organisation has correct fields');
       t.end();
     });
   });
 });
 
-tape('teardown orgsAdd.test.js', t => {
+// tape('teardown orgsAdd.test.js', t => {
+//   t.end();
+//   client.end(true);
+//   server.stop(() => {});
+// });
+
+tape.onFinish(() => {
   client.FLUSHDB();
-  client.end(true);
-  server.stop(() => {});
-  t.end();
+  process.exit(0);
 });
