ba0e55155871273d57d5195698fa6178a172e85d
mk4111 <marie.kasai@live.co.uk>
2016-08-22 22:44:08 +0100

    test/index: filename change fixed

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-require('./server.test.js');
+require('./server.js');
 require('./people/admin-adds-primary-user.js');
 require('./people/primary-user-activates-account.js');
 // require('./organisations/admin-adds-org.js');
