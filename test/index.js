fd03085bbcd27c6b91183c6822a6b119e6a2039e
mk4111 <marie.kasai@live.co.uk>
2016-08-22 22:34:12 +0100

    test/index.js: filenames changed to reflect relevant change in naming

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
 require('./server.test.js');
-require('./usersAdd.test.js');
-// require('./orgsAdd.test.js');
-require('./usersActivate.test.js');
+require('./people/admin-adds-primary-user.js');
+require('./people/primary-user-activates-account.js');
+// require('./organisations/admin-adds-org.js');
