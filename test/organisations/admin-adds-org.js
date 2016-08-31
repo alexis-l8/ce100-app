ef331cb140ebdb81de570c82671d77ca1a4969a3
mk4111 <marie.kasai@live.co.uk>
2016-08-31 18:05:23 +0100

    '#46 - test/admin-adds-org: removes setup test as no longer needed.

--- test/organisations/admin-adds-org.js
+++ test/organisations/admin-adds-org.js
@@ -6,34 +6,20 @@ const mockData = require('../mock-data.js');
 
 // TODO: Route should be authed
 
-tape('set orgsAdd.test.js', t => {
-  // Set up db
-  t.end();
-});
-
-tape('/orgs/add load page', t => {
-  const options = {
-    method: 'GET',
-    url: '/orgs/add'
-  };
-  server.inject(options, reply => {
-    console.log(reply);
-    t.equal(reply.statusCode, 200, 'route exists and replies 200');
-    t.end();
-  });
-});
-
-tape('orgs/add posts new organisation details', t => {
+tape('orgs/add adds a new organisation', t => {
   t.plan(2);
   const options = {
     method: 'POST',
     url: '/orgs/add',
     payload: JSON.stringify(mockData.orgsAddPayload)
   };
+  // hit endpoint with mock form
   server.inject(options, reply => {
-    t.equal(reply.statusCode, 200, 'route exists and replies 200');
-    client.LRANGE('organisations', 0, -1, (error, organisations) => {
-      t.deepEqual(JSON.parse(organisations[0]), mockData.orgsAddDB, 'new organisation has been stored in the DB with correct fields');
+    t.equal(200, reply.statusCode, 'route exists and replies 200');
+    // check organisation has been added to db
+    client.LRANGE('organisations', (error, orgs) => {
+      console.log('ERROR', error);
+      t.deepEqual(JSON.parse(orgs[0]), mockData.orgsAddDB, 'new organisation has correct fields');
       t.end();
     });
   });
