989f950a02306c5b4e5ab359256a92f42b5c0519
mk4111 <marie.kasai@live.co.uk>
2016-08-31 17:35:16 +0100

    #46 - test/index: adds admin-adds-org test to list

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 require('./server.js');
+require('./organisations/admin-adds-org.js');
 require('./people/admin-adds-primary-user.js');
 require('./people/primary-user-activates-account.js');
-// require('./organisations/admin-adds-org.js');
+require('./people/primary-user-log-in.js');
