a919d8df1f5fa622270cccfedc1311e1aff821b7
Marie Kasai <marie.kasai@live.co.uk>
2016-09-04 19:07:51 +0100

    #75 - test/index: comments out all but one test

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
 require('./server.js');
-require('./people/admin-adds-primary-user.js');
-require('./people/primary-user-activates-account.js');
+// require('./people/admin-adds-primary-user.js');
+// require('./people/primary-user-activates-account.js');
 // require('./organisations/admin-adds-org.js');
