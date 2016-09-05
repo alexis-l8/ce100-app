8785fc842f5b7ebefab32d77379bdaec9af5192a
Marie Kasai <marie.kasai@live.co.uk>
2016-09-05 13:24:05 +0100

    removes untested /login function and console logs

--- server/handlers.js
+++ server/handlers.js
@@ -1,9 +1,6 @@
 // require('env2')('config.env');
 
 const bcrypt = require('bcrypt');
-const Boom = require('boom');
-const Hashids = require('hashids');
-const hash = new Hashids(process.env.HASHID_KEY);
 
 const handlers = {};
 
@@ -31,18 +28,15 @@ handlers.activatePrimaryUser = (request, reply) => {
   // hash password
   bcrypt.hash(request.payload.password, 10, function (error, hashedPassword) {
     if (error) {
-      console.log(error);
       reply('hash failed');
     } else {
       redis.LINDEX('people', userId, (error, user) => {
         if (error) {
-          console.log(error);
           reply('redis-failure');
         } else {
           const updatedUser = addPasswordToUser(hashedPassword, user);
           redis.LSET('people', userId, updatedUser, (err, response) => {
             if (err) {
-              console.log(err);
               reply('redis-failure');
             } else {
               reply('OK').state('CEsession', hashedId, cookieConfig); // view dashboard and set new cookie
@@ -60,25 +54,21 @@ handlers.createNewPrimaryUser = (request, reply) => {
   delete payload.submit;
   redis.LLEN('people', (error, length) => {
     if (error) {
-      console.log(error);
       reply('redis-failure');
     } else {
       const userUpdated = initialiseNewUser(length, payload);
       redis.RPUSH('people', userUpdated, (error, people) => {
         if (error) {
-          console.log('ERROR', error);
           reply('redis-failure');
         } else {
           const orgId = payload.organisation_id;
           redis.LINDEX('organisations', orgId, (error, org) => {
             if (error) {
-              console.log('ERROR', error);
               reply('redis-failure');
             } else {
               const orgUpdated = addPrimaryToOrg(userUpdated, org);
               redis.LSET('organisations', orgId, orgUpdated, (error, response) => {
                 if (error) {
-                  console.log('ERROR', error);
                   reply('redis-failure');
                 } else {
                   reply('success');
@@ -88,34 +78,6 @@ handlers.createNewPrimaryUser = (request, reply) => {
           });
         }
       });
-    }
-  });
-};
-
-handlers.login = (request, reply) => {
-  const email = request.payload.email;
-  const password = request.payload.password;
-  request.redis.LRANGE('people', 0, -1, (error, allUsers) => {
-    if (error) {
-      console.log('ERROR', error);
-      reply('redis-failure');
-    } else {
-      const user = allUsers.filter((eachUser, index) => {
-        return JSON.parse(eachUser).email === email;
-      });
-      const userDetails = JSON.parse(user);
-      if (userDetails) {
-        bcrypt.compare(password, userDetails.password, function (err, isValid) {
-          if (!err && isValid) {
-            // TODO: update last login
-            reply('OK').state('CEsession', hash.encode(userDetails.id), cookieConfig); // view dashboard
-          } else {
-            reply(Boom.notFound('Sorry, that email or password is invalid, please try again.'));
-          }
-        });
-      } else {
-        reply(Boom.notFound('Sorry, that email has not been registered.'));
-      }
     }
   });
 };
