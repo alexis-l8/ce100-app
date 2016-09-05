339159265839094d885a6403b5260f97d6827e5c
mk4111 <marie.kasai@live.co.uk>
2016-09-05 22:01:33 +0100

    #88 - admin-adds-primary-user.test: adds catches for errors

--- test/admin-adds-primary-user.test.js
+++ test/admin-adds-primary-user.test.js
@@ -7,8 +7,12 @@ const mockData = require('./mock-data.js');
 // TODO: Route should be authed
 
 tape('set up db', t => {
-  client.RPUSH('organisations', JSON.stringify(mockData.orgsAddDB), (err, data) => {
-    console.log('setup: redis response to org added: ', data);
+  client.RPUSH('organisations', JSON.stringify(mockData.orgsAddDB), (error, data) => {
+    if (error) {
+      console.log('ERROR', error);
+    } else {
+      console.log('setup: redis response to org added: ', data);
+    }
     t.end();
   });
 });
@@ -22,13 +26,21 @@ tape('/add-user post adds a user and updates the linked organisation', (t) => {
   };
   server.inject(options, reply => {
     t.equal(reply.statusCode, 200, 'route exists and replies 200');
-    //check user added to db
+    // check user added to db
     client.LRANGE('people', 0, -1, (error, people) => {
-      t.deepEqual(JSON.parse(people[0]), mockData.newUserAdded, 'the new user has correct fields');
-      client.LRANGE('organisations', 0, -1, (error, orgs) => {
-        t.deepEqual(JSON.parse(orgs[0]), mockData.orgPostUser, 'Primary user added to organisation');
-        t.end();
-      });
+      if (error) {
+        console.log('ERROR', error);
+      } else {
+        t.deepEqual(JSON.parse(people[0]), mockData.newUserAdded, 'the new user has correct fields');
+        client.LRANGE('organisations', 0, -1, (error, orgs) => {
+          if (error) {
+            console.log('ERROR', error);
+          } else {
+            t.deepEqual(JSON.parse(orgs[0]), mockData.orgPostUser, 'Primary user added to organisation');
+            t.end();
+          }
+        });
+      }
     });
   });
 });
