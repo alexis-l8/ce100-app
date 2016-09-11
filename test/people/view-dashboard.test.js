2c86859f561bf107da700a5edcdea3ef42a2ae5b
Marie Kasai <marie.kasai@live.co.uk>
2016-09-11 17:56:19 +0100

    #122 - test/view-dashboard: adds test to get root

---
+++ test/people/view-dashboard.test.js
@@ -0,0 +1,34 @@
+var tape = require('tape');
+var client = require('redis').createClient();
+var server = require('../../server/server.js');
+var setup = require('../helpers/set-up.js');
+
+var jwt = require('jsonwebtoken');
+var admin_token = jwt.sign({userId: 0}, process.env.JWT_SECRET);
+var primary_token = jwt.sign({userId: 2}, process.env.JWT_SECRET);
+
+tape('set up: initialise db', t => {
+  setup.initialiseDB(t.end);
+});
+
+tape('root (dashboard) page loads', t => {
+  var options = {
+    method: 'GET',
+    url: '/',
+    headers: { cookie: `token=${admin_token}` }
+  };
+  server.inject(options, reply => {
+    t.equal(reply.statusCode, 200, 'route exists and replies 200');
+    t.end();
+  });
+});
+
+tape('teardown', t => {
+  client.FLUSHDB();
+  t.end();
+});
+
+tape.onFinish(() => {
+  client.end(true);
+  server.stop(() => {});
+});
