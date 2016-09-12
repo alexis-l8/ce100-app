528b890decc29d701b40b62d01a9bde167d2eab4
nelsonic <contact.nelsonic+github@gmail.com>
2016-09-12 02:14:28 +0100

    fixes require('Hoek') >> require('hoek') Case Sensitivegit add .

--- server/email.js
+++ server/email.js
@@ -1,12 +1,12 @@
 var email = require('sendemail');
 email.set_template_directory('server/email-templates');
 
-var Hoek = require('Hoek');
+var Hoek = require('hoek');
 var sendEmail = {};
 
 sendEmail.newUser = (person, callback) => {
   const recipient = Object.assign({}, person, { subject: 'Welcome to CE100!' });
-  
+
   // someone please add sender email address to .env Google Doc! then uncomment
   // see: https://github.com/dwyl/sendemail#2-set-your-environment-variables
 
