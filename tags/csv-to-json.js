9a23814c139fb3aa98f3bbeb4f40a38d355134fb
mk4111 <marie.kasai@live.co.uk>
2016-09-14 01:21:59 +0100

    #105 - csv-to-json: creates file that translates .csv --> .json format

---
+++ tags/csv-to-json.js
@@ -0,0 +1,41 @@
+var Hoek = require('hoek');
+var fs = require('fs');
+var path = require('path');
+
+function csvToJson (fileName) {
+  fs.readFile(path.join(__dirname, fileName), (error, data) => {
+    Hoek.assert(!error, error);
+    var csv = data.toString().split(/\n/g);
+    var allTags = csv[0].split(/,/g).map((header, index) => {
+      return {
+        id: index,
+        name: header,
+        enabled: true,
+        tags: []
+      };
+    });
+    var body = csv.slice(1);
+    body.forEach((row, rowLength) => {
+      var tags = row.split(/,/g);
+      tags.forEach((tag, columnLength) => {
+        if (tag) {
+          allTags[columnLength].tags.push({
+            id: rowLength,
+            name: tag,
+            parent_id: columnLength,
+            enabled: true
+          });
+        }
+        if (rowLength === body.length - 1) {
+          var jsonFile = fileName.split(/\./g).shift() + '.json';
+          fs.writeFile(path.join(__dirname, jsonFile), JSON.stringify(allTags), (error, response) => {
+            Hoek.assert(!error, error);
+          });
+        }
+      });
+    });
+  });
+}
+
+csvToJson('/members.csv');
+csvToJson('/topics.csv');
