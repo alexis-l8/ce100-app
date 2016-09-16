c242373fbe67ba8236e90b66cff671779e90eaec
mk4111 <marie.kasai@live.co.uk>
2016-09-16 09:34:09 +0100

    #151 - handlers/select-tags: creates select-tags handler for /tags POST route

---
+++ server/handlers/select-tags.js
@@ -0,0 +1,8 @@
+var Hoek = require('hoek');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = (request, reply) => {
+  // var selectedTags = request.payload.tags;
+  // console.log(selectedTags);
+};
