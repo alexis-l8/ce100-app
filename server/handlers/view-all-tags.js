018482cf35c90c314f8a1678e1493b725b9f4535
mk4111 <marie.kasai@live.co.uk>
2016-09-16 09:28:19 +0100

    #151 - view-all-tags: refactors code to read in contents of all tags/*.json files

--- server/handlers/view-all-tags.js
+++ server/handlers/view-all-tags.js
@@ -1,9 +1,14 @@
 var Hoek = require('hoek');
+var fs = require('fs');
+var path = require('path');
 
 module.exports = (request, reply) => {
-  var tags = {
-    topics: require('../../tags/topics.json'),
-    members: require('../../tags/members.json')
-  };
-  reply.view('tags', tags);
+  var tagsdir = path.join(__dirname, '../../tags/');
+  fs.readdir(`${tagsdir}/`, (error, files) => {
+    Hoek.assert(!error, error);
+    var jsonFiles = files.filter(file => file.substr(-5) === ('.json'))
+                         .map(json => require(`${tagsdir}/${json}`))
+                         .reduce((prev, current) => prev.concat(current));
+    reply.view('tags', { tags: jsonFiles });
+  });
 };
