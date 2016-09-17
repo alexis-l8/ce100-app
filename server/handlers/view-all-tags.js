adfe77362c47ae029fdaaa433ed2cf7407d9b414
mk4111 <marie.kasai@live.co.uk>
2016-09-17 14:15:52 +0100

    #164 - handlers/view-all-tags: adapts handler following bug fix #164

--- server/handlers/view-all-tags.js
+++ server/handlers/view-all-tags.js
@@ -1,14 +1,3 @@
-var Hoek = require('hoek');
-var fs = require('fs');
-var path = require('path');
-
 module.exports = (request, reply) => {
-  var tagsdir = path.join(__dirname, '../../tags/');
-  fs.readdir(`${tagsdir}/`, (error, files) => {
-    Hoek.assert(!error, error);
-    var jsonFiles = files.filter(file => file.substr(-5) === ('.json'))
-                         .map(json => require(`${tagsdir}/${json}`))
-                         .reduce((prev, current) => prev.concat(current));
-    reply.view('tags', { tags: jsonFiles });
-  });
+  reply.view('tags', { parent_tags: require('../../tags/tags.json') });
 };
