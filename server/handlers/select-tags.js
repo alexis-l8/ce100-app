36408aa513877fbc6b3abe89f06a8a82d703b66a
mk4111 <marie.kasai@live.co.uk>
2016-09-19 01:14:49 +0100

    #61 - handlers/select-tags: removed

--- server/handlers/select-tags.js
+++
@@ -1,8 +0,0 @@
-var Hoek = require('hoek');
-var fs = require('fs');
-var path = require('path');
-
-module.exports = (request, reply) => {
-  // var selectedTags = request.payload.tags;
-  // console.log(selectedTags);
-};
