2252b54f0d1c7bd36388d055fe4a5296078edbf4
Marie Kasai <marie.kasai@live.co.uk>
2016-09-21 01:39:40 +0100

    #12 - handlers/add-new-challenge: changes method that challenge is saved

--- server/handlers/add-new-challenge.js
+++ server/handlers/add-new-challenge.js
@@ -4,45 +4,35 @@ module.exports = (request, reply) => {
   var payload = request.payload;
   var userId = request.auth.credentials.userId;
   Hoek.assert(request.auth.credentials.scope === 'primary', 'Admins cannot create a new challenge as no organisation is attached.');
-  // if (payload['add-tags']) {
-  //   var options = {
-  //     parent_tags: require('../../tags/tags.json'),
-  //     hide: payload
-  //   };
-  //   reply.view('tags', options);
-  // } else {
-    request.redis.LINDEX('people', userId, (error, stringifiedUser) => {
+  request.redis.LINDEX('people', userId, (error, stringifiedUser) => {
+    Hoek.assert(!error, error);
+    var user = JSON.parse(stringifiedUser);
+    var orgId = user.organisation_id;
+    request.redis.LINDEX('organisations', orgId, (error, stringifiedOrg) => {
       Hoek.assert(!error, error);
-      var user = JSON.parse(stringifiedUser);
-      var orgId = user.organisation_id;
-      request.redis.LINDEX('organisations', orgId, (error, stringifiedOrg) => {
+      var org = JSON.parse(stringifiedOrg);
+      request.redis.LLEN('challenges', (error, challengeId) => {
         Hoek.assert(!error, error);
-        var org = JSON.parse(stringifiedOrg);
-        request.redis.LLEN('challenges', (error, challengeId) => {
+        user.challenges.push(challengeId);
+        org.challenges.push(challengeId);
+        var challenge = Object.assign({}, payload, {
+          id: challengeId,
+          org_id: orgId,
+          creator_id: userId,
+          date: Date.now(),
+          archived: false
+        });
+        request.redis.RPUSH('challenges', JSON.stringify(challenge), (error, response) => {
           Hoek.assert(!error, error);
-          user.challenges.push(challengeId);
-          org.challenges.push(challengeId);
-          var challenge = Object.assign({}, payload, {
-            id: challengeId,
-            org_id: orgId,
-            creator_id: userId,
-            date: Date.now(),
-            tags: [],
-            // tags: payload.tags && payload.tags !== '-1' ? payload.tags.map(tag => JSON.parse(tag)) : [],
-            archived: false
-          });
-          request.redis.RPUSH('challenges', JSON.stringify(challenge), (error, response) => {
+          request.redis.LSET('people', userId, JSON.stringify(user), (error, response) => {
             Hoek.assert(!error, error);
-            request.redis.LSET('people', userId, JSON.stringify(user), (error, response) => {
+            request.redis.LSET('organisations', orgId, JSON.stringify(org), (error, response) => {
               Hoek.assert(!error, error);
-              request.redis.LSET('organisations', orgId, JSON.stringify(org), (error, response) => {
-                Hoek.assert(!error, error);
-                reply({orgId: orgId}).redirect(`/orgs/${orgId}`);
-              });
+              reply({challengeId: challengeId}).redirect(`/challenges/${challengeId}/tags`);
             });
           });
         });
       });
     });
-  // }
+  });
 };
