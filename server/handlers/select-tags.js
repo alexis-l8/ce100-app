93feee958f6c8ae0ba6429ed4944275ca4923b9d
Marie Kasai <marie.kasai@live.co.uk>
2016-09-21 13:07:44 +0100

    #171 - handlers/select-tags: nested ternary --> if/else

--- server/handlers/select-tags.js
+++ server/handlers/select-tags.js
@@ -3,9 +3,10 @@ var Hoek = require('hoek');
 module.exports = (request, reply) => {
   var challengeId = request.params.challengeId;
   var payload = request.payload.tags;
-  var tags = payload && payload !== -1
-    ? (typeof payload === 'string' ? [JSON.parse(payload)] : payload.map(tag => JSON.parse(tag)))
-    : [];
+  var tags;
+  if (payload && payload !== -1) {
+    tags = typeof payload === 'string' ? [JSON.parse(payload)] : payload.map(tag => JSON.parse(tag));
+  } else tags = [];
   request.redis.LINDEX('challenges', challengeId, (error, stringifiedChallenge) => {
     Hoek.assert(!error, 'redis error');
     var challenge = JSON.parse(stringifiedChallenge);
