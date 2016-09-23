9354760ecf581df3e6324e3aa0d2ae215a1ea5fd
J Murphy <jmurphy.web@gmail.com>
2016-09-23 10:05:56 +0100

    remove edit functionality from quick contact list #27

--- server/handlers/all-users-view.js
+++ server/handlers/all-users-view.js
@@ -9,19 +9,9 @@ module.exports = (request, reply) => {
     Hoek.assert(!error, 'redis error');
     request.redis.LRANGE('organisations', 0, -1, (error, stringifiedOrgs) => {
       Hoek.assert(!error, 'redis error');
-      var orgs = stringifiedOrgs.map(element => JSON.parse(element));
-      var users = stringifiedUsers.map(u => {
-        var user = JSON.parse(u);
-        var additionalInfo = {
-          organisation_id: user.organisation_id > -1
-            ? orgs[user.organisation_id].name
-            : false
-        };
-        return Object.assign(additionalInfo, user);
-      });
-      // TODO: Can this navbar logic go somewhere else?
-      var allUsers = {
-        allUsers: users,
+      var allUsers = attachOrgsToUsers(stringifiedOrgs, stringifiedUsers);
+      var users = {
+        allUsers: loggedIn.scope === 'admin' ? allUsers : filterActiveAndAdmin(allUsers),
         alternate: [{
           path: '/people/add',
           name: '+'
@@ -30,8 +20,25 @@ module.exports = (request, reply) => {
           name: 'Orgs'
         }]
       };
-      var options = Object.assign({}, allUsers, permissions);
+      var options = Object.assign({}, users, permissions);
       reply.view('people/view', options);
     });
   });
 };
+
+function filterActiveAndAdmin (arr) {
+  return arr.filter(el => el.active && el.user_type !== 'admin');
+}
+
+function attachOrgsToUsers (stringifiedOrgs, stringifiedUsers) {
+  var orgs = stringifiedOrgs.map(element => JSON.parse(element));
+  return stringifiedUsers.map(u => {
+    var user = JSON.parse(u);
+    var additionalInfo = {
+      organisation_id: user.organisation_id > -1
+        ? orgs[user.organisation_id].name
+        : false
+    };
+    return Object.assign(additionalInfo, user);
+  })
+}
