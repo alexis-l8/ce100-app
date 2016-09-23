1d258687c7949126aa5ce5c52e2e2628295d94b8
Marie Kasai <marie.kasai@live.co.uk>
2016-09-23 12:42:21 +0100

    cherry picked from 76a18c05f24d6eb6649c45ee28222af82628e3e2

--- server/handlers/org-details-view.js
+++ server/handlers/org-details-view.js
@@ -12,27 +12,50 @@ module.exports = (request, reply) => {
     Hoek.assert(!error, 'redis error');
     // TODO: catch for case where org at specified userId doesn't exist.
     var organisation = JSON.parse(stringifiedOrg);
-    var orgDetails = Object.assign({}, organisation, permissions);
-    if (organisation.primary_id === -1 && organisation.challenges.length === 0) {
-      return reply.view('organisations/details', { organisation: organisation });
-    } else {
-      request.redis.LINDEX('people', organisation.primary_id, (error, stringifiedPrimaryUser) => {
+    // get all challenges
+    request.redis.LRANGE('challenges', 0, -1, (error, challengesList) => {
+      Hoek.assert(!error, 'redis error');
+      var challenges = getChallenges(challengesList, organisation);
+
+      // if no primary user then reply
+      if (organisation.primary_id === -1) {
+        var options = Object.assign({}, {challenges}, {organisation}, permissions);
+        return reply.view('organisations/details', options);
+      }
+
+      // else get linked primary user and reply
+      request.redis.LINDEX('people', organisation.primary_id, (error, stringifiedUser) => {
         Hoek.assert(!error, 'redis error');
-        var {first_name, last_name, email, phone, job} = JSON.parse(stringifiedPrimaryUser);
-        orgDetails.primary_user = Object.assign({first_name, last_name, email, phone, job});
-        if (organisation.challenges.length > 0) {
-          request.redis.LRANGE('challenges', 0, -1, (error, challenges) => {
-            Hoek.assert(!error, 'redis error');
-            orgDetails.challenges = organisation.challenges.map(challengeId => {
-              return JSON.parse(challenges[challengeId]);
-            });
-            reply.view('organisations/details', orgDetails);
-          });
-        } else {
-          orgDetails.challenges = false;
-          reply.view('organisations/details', orgDetails);
-        }
+        var primary_user = getUserInfo(stringifiedUser);
+        var options = Object.assign({}, {primary_user}, {challenges}, {organisation}, permissions);
+        return reply.view('organisations/details', options);
       });
-    }
+    });
   });
 };
+
+function getUserInfo (stringifiedUser) {
+  var { first_name, last_name, email, phone, job_title } = JSON.parse(stringifiedUser);
+  return {first_name, last_name, email, phone, job_title};
+}
+
+function getChallenges (challengesList, organisation) {
+  var challengeArr = organisation.challenges.map((challengeId, index) => {
+    var challengeCard = JSON.parse(challengesList[challengeId]);
+    var tagsArray = getTagNames(challengeCard.tags);
+    return Object.assign({}, challengeCard, {tags: tagsArray});
+  });
+  return challengeArr.length === 0 ? false : challengeArr;
+}
+
+function getTagNames (tagIds) {
+  var allTags = require('../../tags/tags.json');
+  return tagIds;
+  // This will make sense in a future commit. Has been cherry picked from 76a18c05f24d6eb6649c45ee28222af82628e3e2
+  // return tagIds.map(tagId => {
+  //   return {
+  //     id: tagId,
+  //     name: allTags[tagId[0]].tags[tagId[1]].name
+  //   };
+  // });
+}
