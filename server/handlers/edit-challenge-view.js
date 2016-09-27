e1cdaed2d5d557115d3835f09801e28d6aa10b6e
Marie Kasai <marie.kasai@live.co.uk>
2016-09-27 10:19:11 +0100

    #198 - edit-challenge-view: fixes typo

--- server/handlers/edit-challenge-view.js
+++ server/handlers/edit-challenge-view.js
@@ -7,7 +7,7 @@ module.exports = (request, reply) => {
     Hoek.assert(!error, 'redis error');
     var challenge = JSON.parse(stringifiedChallenge);
     if (challenge.tags.length === 0) {
-      return reply.view('challenge/edit', challenge);
+      return reply.view('challenges/edit', challenge);
     } else {
       var tags = { tags: helpers.getTagNames(challenge.tags) };
       var options = Object.assign({}, challenge, tags);
