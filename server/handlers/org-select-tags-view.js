4e64cb74de9b21dad73c2ee17330df72e853f997
Marie Kasai <marie.kasai@live.co.uk>
2016-09-28 17:11:11 +0100

    #208 - handlers/select-tags-view && handlers/org-select-tags-view: require(.json) --> fs.readFile(.json)

--- server/handlers/org-select-tags-view.js
+++ server/handlers/org-select-tags-view.js
@@ -1,16 +1,23 @@
 var Hoek = require('hoek');
+var fs = require('fs');
+var path = require('path');
 
 module.exports = (request, reply) => {
   request.redis.LINDEX('organisations', request.params.id, (error, stringifiedOrg) => {
     Hoek.assert(!error, error);
     var org = JSON.parse(stringifiedOrg);
-    var allTags = require('../../tags/tags.json');
-    if (org.tags) {
-      org.tags.forEach((tag, index) => {
-        allTags[tag[0]].selected = true;
-        allTags[tag[0]].tags[tag[1]].selected = true;
-      });
-    }
-    reply.view('tags', { parent_tags: allTags });
+    fs.readFile(path.join(__dirname, '../../tags/tags.json'), 'utf8', (error, data) => {
+      Hoek.assert(!error, error);
+      var allTags = JSON.parse(data);
+      if (org.tags) {
+        org.tags.forEach((tag, index) => {
+          allTags[tag[0]].selected = true;
+          allTags[tag[0]].tags[tag[1]].selected = true;
+        });
+      }
+      var permissions = require('./helpers.js').getPermissions(request.auth.credentials, 'scope', 'admin');
+      var options = Object.assign({ parent_tags: allTags }, permissions);
+      reply.view('tags', options);
+    });
   });
 };
