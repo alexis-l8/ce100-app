99b0ede7efed5a2aaf65bf85c68045c761afc7ca
J Murphy <jmurphy.web@gmail.com>
2016-10-03 19:03:29 +0100

    added new failaction function to login #32

--- server/models/login.js
+++ server/models/login.js
@@ -5,16 +5,19 @@ module.exports = {
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required()
   },
-  failAction: failAction
+  failAction: failAction('login')
 };
 
-function failAction (request, reply, source, error) {
-  reply.view('login', {error: errorOptions(error)}).code(401);
+function failAction (view) {
+  return function (request, reply, source, error) {
+    reply.view(view, {error: errorOptions(error)}).code(401);
+  };
 }
 
 function errorOptions (err) {
   return {
     values: err.data._object,
-    message: err.data.details[0].message.split('"').join('').split('_').join('').split('-').join('')
+    message: err.data.details[0].message.split('"').join('').split('_').join('').split('-').join(''),
+    [err.data.details[0].path]: 'form__input-error'
   };
 }
