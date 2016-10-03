1ae0c728366ef5e0076b504c24d091ac58969dcc
J Murphy <jmurphy.web@gmail.com>
2016-10-03 18:47:47 +0100

    implement snack-bar, add to orgs/add view #32

--- test/organisations/admin-adds-org.test.js
+++ test/organisations/admin-adds-org.test.js
@@ -25,37 +25,46 @@ tape('orgs/add view', t => {
     url: '/orgs/add',
     headers: { cookie: `token=${primary_token}` }
   };
-  server.inject(primaryCookie, reply => {
-    t.equal(reply.statusCode, 403, 'unauthorised user cannot access the route');
-    server.inject(adminCookie, reply => {
-      t.equal(reply.statusCode, 200, 'admin can access the route');
+  server.inject(primaryCookie, res => {
+    t.equal(res.statusCode, 403, 'unauthorised user cannot access the route');
+    server.inject(adminCookie, res => {
+      t.equal(res.statusCode, 200, 'admin can access the route');
       t.end();
     });
   });
 });
 
 tape('orgs/add admin adds a new organisation', t => {
-  t.plan(3);
+  var noName = {
+    method: 'POST',
+    url: '/orgs/add',
+    payload: {name: ''},
+    headers: { cookie: `token=${admin_token}` }
+  }
   var options = {
     method: 'POST',
     url: '/orgs/add',
     payload: JSON.stringify(payloads.orgsAddPayload),
     headers: { cookie: `token=${admin_token}` }
   };
-  server.inject(options, reply => {
-    t.equal(reply.statusCode, 302, 'admin is redirected');
-    var url = reply.headers.location;
-    t.ok(url === ('/browse/orgs'), 'redirected to the new organisations view');
-    var options2 = {
-      method: 'GET',
-      url: '/browse/orgs',
-      headers: { cookie: `token=${admin_token}` }
-    };
-    server.inject(options2, reply => {
-      t.ok(reply.payload.indexOf(payloads.orgsAddPayload.name) > -1, 'mock organisation was added');
-      t.end();
+  var options2 = {
+    method: 'GET',
+    url: '/browse/orgs',
+    headers: { cookie: `token=${admin_token}` }
+  };
+  server.inject(noName, res => {
+    t.equal(res.statusCode, 401, 'failing validation returns status code 401');
+    t.ok(res.payload.indexOf('name is not allowed to be empty') > -1, 'if admin fails validation we show them appropriate message: "name is not allowed to be empty"');
+    server.inject(options, res => {
+      t.equal(res.statusCode, 302, 'admin is redirected');
+      var url = res.headers.location;
+      t.ok(url === ('/browse/orgs'), 'redirected to the new organisations view');
+      server.inject(options2, res => {
+        t.ok(res.payload.indexOf(payloads.orgsAddPayload.name) > -1, 'mock organisation was added');
+        t.end();
+      });
     });
-  });
+  })
 });
 
 tape('teardown', t => {
