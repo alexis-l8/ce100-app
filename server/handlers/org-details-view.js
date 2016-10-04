a480162bd17b05e55a531a5e114f2006508ae94c
mk4111 <marie.kasai@live.co.uk>
2016-10-04 17:21:05 +0100

    #211 - handlers/org-details-view: changes sync --> async following getTagNames change

--- server/handlers/org-details-view.js
+++ server/handlers/org-details-view.js
@@ -11,26 +11,28 @@ module.exports = (request, reply) => {
     Hoek.assert(!error, 'redis error');
     // TODO: catch for case where org at specified userId doesn't exist.
     var organisation = JSON.parse(stringifiedOrg);
-    var organisationTags = organisation.tags && helpers.getTagNames(organisation.tags);
-    organisation.tags = organisationTags;
+    helpers.getTagNames(request.redis, organisation.tags, organisationTags => {
+      organisation.tags = organisationTags;
 
-    // get all challenges
-    request.redis.LRANGE('challenges', 0, -1, (error, challengesList) => {
-      Hoek.assert(!error, 'redis error');
-      var challenges = getChallenges(challengesList, organisation.challenges);
-
-      // if no primary user then reply
-      if (organisation.primary_id === -1) {
-        var options = Object.assign({}, {challenges}, {organisation}, permissions);
-        return reply.view('organisations/details', options);
-      }
-
-      // else get linked primary user and reply
-      request.redis.LINDEX('people', organisation.primary_id, (error, stringifiedUser) => {
+      // get all challenges
+      request.redis.LRANGE('challenges', 0, -1, (error, challengesList) => {
         Hoek.assert(!error, 'redis error');
-        var primary_user = getUserInfo(stringifiedUser);
-        var options = Object.assign({}, {primary_user}, {challenges}, {organisation}, permissions);
-        return reply.view('organisations/details', options);
+        getChallenges(request.redis, challengesList, organisation.challenges, challenges => {
+
+          // if no primary user then reply
+          if (organisation.primary_id === -1) {
+            var options = Object.assign({}, {challenges}, {organisation}, permissions);
+            return reply.view('organisations/details', options);
+          }
+
+          // else get linked primary user and reply
+          request.redis.LINDEX('people', organisation.primary_id, (error, stringifiedUser) => {
+            Hoek.assert(!error, 'redis error');
+            var primary_user = getUserInfo(stringifiedUser);
+            var options = Object.assign({}, {primary_user}, {challenges}, {organisation}, permissions);
+            return reply.view('organisations/details', options);
+          });
+        });
       });
     });
   });
@@ -41,12 +43,22 @@ function getUserInfo (stringifiedUser) {
   return {first_name, last_name, email, phone, job_title};
 }
 
-function getChallenges (challengesList, organisationChallenges) {
-  var challengeArr = organisationChallenges.map((challengeId, index) => {
-    var challengeCard = JSON.parse(challengesList[challengeId]);
-    var tagsArray = helpers.getTagNames(challengeCard.tags);
-    return Object.assign({}, challengeCard, {tags: tagsArray});
+function getChallenges (redis, challengesList, organisationChallenges, callback) {
+  var Hoek = require('hoek');
+  redis.HGET('tags', 'tags', (error, response) => {
+    Hoek.assert(!error, error);
+    var allTags = JSON.parse(response);
+    var challengeArr = organisationChallenges.map((challengeId, index) => {
+      var challengeCard = JSON.parse(challengesList[challengeId]);
+      var tagsArray = challengeCard.tags && challengeCard.tags.map(tagId => {
+        return {
+          id: tagId,
+          name: allTags[tagId[0]].tags[tagId[1]].name
+        };
+      });
+      return Object.assign({}, challengeCard, {tags: tagsArray});
+    });
+    var activeChallenges = challengeArr.filter(challenge => challenge.active);
+    challengeArr.length === 0 ? callback(false) : callback(activeChallenges);
   });
-  var activeChallenges = challengeArr.filter(challenge => challenge.active);
-  return challengeArr.length === 0 ? false : activeChallenges;
 }
