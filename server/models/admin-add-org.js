5fe9ff2876494e501c9af2bdd5de5c43a5093d27
J Murphy <jmurphy.web@gmail.com>
2016-10-04 09:52:50 +0100

    add error handling functionality to generic serve-view handler, reuse helpers.errorOptions in all Joi model files #32

--- server/models/admin-add-org.js
+++ server/models/admin-add-org.js
@@ -4,19 +4,5 @@ module.exports = {
   payload: {
     name: Joi.string().min(1).required()
   },
-  failAction: failAction('organisations/add')
+  failAction: require('../handlers/serve-view.js')('organisations/add')
 };
-
-function failAction (view) {
-  return function (request, reply, source, error) {
-    reply.view(view, {error: errorOptions(error)}).code(401);
-  };
-}
-
-function errorOptions (err) {
-  return {
-    values: err.data._object,
-    message: err.data.details[0].message.split('"').join('').split('_').join('').split('-').join(''),
-    [err.data.details[0].path]: 'form__input-error'
-  };
-}
--- server/models/login.js
+++ server/models/login.js
@@ -5,19 +5,5 @@ module.exports = {
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required()
   },
-  failAction: failAction('login')
+  failAction: require('../handlers/serve-view.js')('login')
 };
-
-function failAction (view) {
-  return function (request, reply, source, error) {
-    reply.view(view, {error: errorOptions(error)}).code(401);
-  };
-}
-
-function errorOptions (err) {
-  return {
-    values: err.data._object,
-    message: err.data.details[0].message.split('"').join('').split('_').join('').split('-').join(''),
-    [err.data.details[0].path]: 'form__input-error'
-  };
-}
