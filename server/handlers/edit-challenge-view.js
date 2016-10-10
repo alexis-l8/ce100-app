b37d9e949eda3655e2236aedb501a71c3c6b2f70
J Murphy <jmurphy.web@gmail.com>
2016-10-10 19:26:18 +0100

    fix conflicts

--- server/handlers/edit-challenge-view.js
+++ server/handlers/edit-challenge-view.js
@@ -3,17 +3,12 @@ var helpers = require('./helpers');
 
 module.exports = (request, reply, source, joiErr) => {
   var error = helpers.errorOptions(joiErr);
+  var permissions = helpers.getPermissions(request.auth.credentials, 'scope', 'admin');
   var challengeId = request.params.id;
   request.redis.LINDEX('challenges', challengeId, (redisErr, stringifiedChallenge) => {
     Hoek.assert(!redisErr, 'redis error');
     var challenge = JSON.parse(stringifiedChallenge);
-
-    if (challenge.tags.length === 0) {
-      reply.view('challenges/edit', challenge);
-      return;
-    }
     helpers.getTagNames(request.redis, challenge.tags, tagsData => {
-      var permissions = helpers.getPermissions(request.auth.credentials, 'scope', 'admin');
       var options = Object.assign({}, challenge, {tagsData}, permissions, {error});
       reply.view('challenges/edit', options).code(error ? 401 : 200);
     });
