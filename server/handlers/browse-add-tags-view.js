a58ccc5c2e3e4d1143ab64d90796b0cda64cb2d0
mk4111 <marie.kasai@live.co.uk>
2016-11-17 21:34:38 +0000

    #340 WIP - attempting to start integrating plugin functionality...

--- server/handlers/browse-add-tags-view.js
+++ server/handlers/browse-add-tags-view.js
@@ -1,8 +1,19 @@
+'use strict';
+
 var helpers = require('./helpers.js');
+
+/**
+* Switch out redis --> postgres:
+* Build 'options' object, simplify
+*/
+
 module.exports = (request, reply) => {
   var permissions = helpers.getPermissions(request.auth.credentials);
+  // request.getTags(function (err, allTags) {
+  // });
   request.redis.HGET('tags', 'tags', (error, allTags) => {
     var parent_tags = JSON.parse(allTags);
+    // Define which of the tabs are selected --> 'orgs' or 'challenges' view
     var view = { [request.route.path.split('/')[1]]: true };
     var options = Object.assign({}, {view}, {parent_tags}, permissions);
     reply.view('browse/tags', options);
