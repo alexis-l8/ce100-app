095bab71fa2b578e2ab6064c4279740d08d4a03b
SimonLab <simon.labondance@gmail.com>
2016-11-18 12:56:57 +0000

    wip, display tags from pg, #340, #350

--- server/handlers/orgs/browse-add-tags-view.js
+++ server/handlers/orgs/browse-add-tags-view.js
@@ -2,19 +2,19 @@
 
 var helpers = require('../helpers.js');
 
-/**
-* Switch out redis --> postgres:
-* Build 'options' object, simplify
-*/
-
 module.exports = function (request, reply) {
   var permissions = helpers.getPermissions(request.auth.credentials);
-  // request.getTags(function (err, allTags) {
-  // });
-  request.redis.HGET('tags', 'tags', (error, allTags) => {
-    var parent_tags = JSON.parse(allTags);
-    var searchAll = 'orgs';
-    var options = Object.assign({}, {searchAll}, {parent_tags}, permissions);
-    reply.view('browse/tags', options);
+  var pageType = 'orgs';
+  var options = {};
+
+  request.pg.tags.getAllActive(function (error, allActive) {
+    options.pageType = pageType;
+    options.allActive = allActive;
+    options.permissions = permissions;
+    if (Object.keys(options.allActive).length === 0) {
+      return reply.view('browse/no-tags', options);
+    }
+
+    return reply.view('browse/categories-tags', options);
   });
 };
