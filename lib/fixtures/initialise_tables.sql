4de92f0ccccdf40be4d0b8855d3a32c18ed48488
mk4111 <marie.kasai@live.co.uk>
2016-11-21 15:20:17 +0000

    #354 - adds table creation

--- lib/fixtures/initialise_tables.sql
+++ lib/fixtures/initialise_tables.sql
@@ -1,17 +1,38 @@
 -- Delete tables
 DROP TABLE IF EXISTS tags_challenges CASCADE;
 DROP TABLE IF EXISTS challenges CASCADE;
+DROP TABLE IF EXISTS tags_organisations CASCADE;
 DROP TABLE IF EXISTS organisations CASCADE;
 DROP TABLE IF EXISTS people CASCADE;
 
--- Create table DUMMY ORG TABLE
+-- Create table
 CREATE TABLE IF NOT EXISTS organisations (
-  id SERIAL PRIMARY KEY
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  logo_url TEXT,
+  mission_statement VARCHAR(200),
+  active BOOLEAN NOT NULL
 );
 
--- Create table DUMMY PEOPLE TABLE
+-- Create table
+CREATE TABLE IF NOT EXISTS tags_organisations (
+  orgs_id INTEGER REFERENCES organisations (id),
+  tags_id INTEGER REFERENCES tags (id)
+);
+
+-- Create table
 CREATE TABLE IF NOT EXISTS people (
-  id SERIAL PRIMARY KEY
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  user_type VARCHAR(10) NOT NULL CHECK (user_type IN ('admin', 'primary', 'secondary')),
+  email VARCHAR(100) NOT NULL UNIQUE,
+  phone VARCHAR(100),
+  password VARCHAR(100) NOT NULL,
+  org_id INTEGER REFERENCES organisations (id),
+  job_title VARCHAR(80),
+  last_login BIGINT,
+  active BOOLEAN NOT NULL
 );
 
 -- Create table
@@ -27,6 +48,6 @@ CREATE TABLE IF NOT EXISTS challenges (
 
 -- Create table
 CREATE TABLE IF NOT EXISTS tags_challenges (
-  tag_id INTEGER REFERENCES tags (id),
-  challenge_id INTEGER REFERENCES challenges (id)
+  tags_id INTEGER REFERENCES tags (id),
+  challenges_id INTEGER REFERENCES challenges (id)
 );
