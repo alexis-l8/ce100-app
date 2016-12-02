1b3d1a1e4b3d5bd8e837447f05303b0f84be6021
Jack Murphy <jmurphy.web@gmail.com>
2016-12-02 10:07:49 +0000

    add orgs.getDetails function and start to fix handlebars #405

---
+++ server/handlers/orgs/edit-org-view.js
@@ -0,0 +1,40 @@
+var Hoek = require('hoek');
+var Boom = require('boom');
+var helpers = require('../helpers.js');
+
+module.exports = function (request, reply, source, joiErr) {
+  var orgId = parseInt(request.params.id, 10);
+  var loggedIn = request.auth.credentials;
+  var permissions = helpers.getPermissions(loggedIn, 'organisation_id', orgId);
+  var error = helpers.errorOptions(joiErr);
+  if (loggedIn.organisation_id !== orgId && loggedIn.scope !== 'admin') {
+    return reply(Boom.unauthorized('You do not have permission to edit that organisation.'));
+  }
+
+  request.server.methods.pg.organisations.getDetails(orgId, function (error, orgData) {
+    var options = Object.assign({}, orgData, permissions, {error});
+    console.log(options);
+    reply.view('organisations/edit', options).code(error ? 401 : 200);
+  })
+
+  // request.redis.LINDEX('organisations', orgId, (redisErr, stringifiedOrg) => {
+  //   Hoek.assert(!redisErr, 'redis error');
+  //   var organisation = JSON.parse(stringifiedOrg);
+  //   helpers.getTagNames(request.redis, organisation.tags, organisationTags => {
+  //     organisation.tags = organisationTags;
+  //     if (organisation.primary_id === -1) {
+  //       var options = Object.assign({}, organisation, permissions, {error});
+  //       return reply.view('organisations/edit', options).code(error ? 401 : 200);
+  //     }
+  //     request.redis.LINDEX('people', organisation.primary_id, (redisErr, stringifiedPrimaryUser) => {
+  //       Hoek.assert(!redisErr, 'redis error');
+  //       var {first_name, last_name, id} = JSON.parse(stringifiedPrimaryUser);
+  //       var options = Object.assign({}, organisation, {
+  //         primary_user_name: `${first_name} ${last_name}`,
+  //         primary_user_id: id
+  //       }, permissions, {error});
+  //       reply.view('organisations/edit', options).code(error ? 401 : 200);
+  //     });
+  //   });
+  // });
+};
--- server/routes/orgs.js
+++ server/routes/orgs.js
@@ -2,6 +2,7 @@
 
 var browseAddTagsView = require('../handlers/shared/browse-add-tags-view.js');
 var orgDetailsView = require('../handlers/orgs/details-view.js');
+var editView = require('../handlers/orgs/edit-org-view.js')
 
 module.exports = [
   {
@@ -39,7 +40,7 @@ module.exports = [
   {
     method: 'GET',
     path: '/orgs/{id}/edit',
-    handler: require('../handlers/edit-org-view.js')
+    handler: editView
   },
   {
     method: 'POST',
