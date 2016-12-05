054a873e37465e162a38dd98041bfa8bea1829a7
mk4111 <marie.kasai@live.co.uk>
2016-12-05 17:59:13 +0000

    #416 - adds tests for /challenges/tags

---
+++ test/challenges/add-tags.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var test = require('tape');
+var init = require('../../server/server.js');
+var config = require('../../server/config.js');
+var sessions = require('../helpers/add-sessions.js');
+// var adminToken = sessions.tokens(config.jwt_secret).admin_1;
+var primaryToken = sessions.tokens(config.jwt_secret).primary_3;
+
+test('All active tags displayed', function (t) {
+  sessions.addAll(function () {
+    init(config, function (err, server, pool) {
+      if (err) {
+        console.log('error initialise server', err); // eslint-disable-line
+
+        return t.fail();
+      }
+
+      server.inject({
+        method: 'GET',
+        url: '/challenges/tags',
+        headers: { cookie: 'token=' + primaryToken }
+      }, function (res) {
+        var unexpected = 'Please Login to view that page';
+        var expLink = '<a class="text-decoration" href="/challenges?tags=124">';
+        var expTag = '<span class="tag__label">Telecommunications</span>';
+        t.equal(res.statusCode, 200, 'primary can view all active tags');
+        t.equal(res.result.indexOf(unexpected), -1,
+          'page does not ask you to log in to view the page');
+        t.ok(res.result.indexOf('BIOLOGICAL CYCLE') > -1, 'category exists');
+        t.ok(res.payload.indexOf(expTag) > -1,
+          'tag correctly displayed');
+        t.ok(res.payload.indexOf(expLink) > -1,
+          'link to tag correctly displayed');
+        t.end();
+        pool.end();
+        server.stop();
+      });
+    });
+  });
+});
--- test/runner.js
+++ test/runner.js
@@ -1,13 +1,10 @@
 'use strict';
 
+require('./challenges/add-tags.test.js');
 
 require('./organisations/org-details-view.test.js');
 require('./organisations/edit-org-view.test.js');
 
-
-
 require('./people/login.test.js');
-
-
 
 require('./server/auth.test.js');
