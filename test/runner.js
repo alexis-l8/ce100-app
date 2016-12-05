57d028f7814844aea4c52554eb4ea82968363666
mk4111 <marie.kasai@live.co.uk>
2016-12-05 18:15:55 +0000

    #419 - adds tests for /orgs/tags

--- test/runner.js
+++ test/runner.js
@@ -4,6 +4,7 @@ require('./challenges/add-tags.test.js');
 
 require('./organisations/org-details-view.test.js');
 require('./organisations/edit-org-view.test.js');
+require('./organisations/add-tags.test.js');
 
 require('./people/login.test.js');
 
