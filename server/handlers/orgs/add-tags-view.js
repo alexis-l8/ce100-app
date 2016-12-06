98fdfd49c34dd9d5666ef75f435a8f134a5fec40
Jack Murphy <jmurphy.web@gmail.com>
2016-12-06 17:31:52 +0000

    Prevent incorrect users from adding tags to org #426

--- server/handlers/orgs/add-tags-view.js
+++ server/handlers/orgs/add-tags-view.js
@@ -1,11 +1,18 @@
 var Hoek = require('hoek');
+var Boom = require('boom');
 var helpers = require('../helpers.js');
 
 module.exports = function (request, reply) {
   var orgId = request.params.id;
+  var loggedIn = request.auth.credentials;
+  var permissions = helpers.getPermissions(loggedIn, 'organisation_id', orgId);
+  if (loggedIn.organisation_id !== orgId && loggedIn.scope !== 'admin') {
+    return reply(Boom.unauthorized('You do not have permission to edit that organisation.'));
+  }
+
+
   request.server.methods.pg.tags.getTagsForEdit('organisations', orgId, function (pgErr, tags) {
     Hoek.assert(!pgErr, 'Database Error');
-    var permissions = helpers.getPermissions(request.auth.credentials, 'scope', 'admin');
     var options = Object.assign({tags}, permissions);
 
     return reply.view('tags', options);
