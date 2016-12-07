39cecdb61e925c9677c922dd0e565eff2af2439d
Jack Murphy <jmurphy.web@gmail.com>
2016-12-07 13:29:17 +0000

    #431 fix tests

--- test/organisations/add-tags-to-org.test.js
+++ test/organisations/add-tags-to-org.test.js
@@ -18,18 +18,18 @@ tape('Admin view add tags to org view: --> ' + __filename, function (t) {
     initServer(config, function (error, server, pool) {
       // admin view org with id 1
       server.inject(addTagsToOrg('admin_1', 1), function (res) {
-
+        console.log(res.result);
         // Check Category 3 is open and Tag id 27 is selected
-        t.ok(res.payload.indexOf('id="3" checked="checked"') > -1, 'The third category is open');
+        t.ok(res.payload.indexOf('id="cat_3" checked="checked"') > -1, 'The third category is open');
         t.ok(res.payload.indexOf('value="27" checked="checked">') > -1, 'The correct tag is already selected');
 
         // Check Category 1 is open and Tag id 1 is selected
-        t.ok(res.payload.indexOf('id="1" checked="checked"') > -1, 'The first category is open');
+        t.ok(res.payload.indexOf('id="cat_1" checked="checked"') > -1, 'The first category is open');
         t.ok(res.payload.indexOf('value="1" checked="checked">') > -1, 'The first tag is already selected');
 
         // To (ever so slightly) improve the validity of these tests,
         // check that category 2 is not open and tag 2 is not checked
-        t.equal(res.payload.indexOf('id="2" checked="checked"'), -1, 'The second category is not open');
+        t.equal(res.payload.indexOf('id="cat_2" checked="checked"'), -1, 'The second category is not open');
         t.equal(res.payload.indexOf('value="2" checked="checked">'), -1, 'The second tag is not selected');
 
         t.end();
@@ -50,16 +50,16 @@ tape('Primary view add tags to org view: --> ' + __filename, function (t) {
         t.equal(res.statusCode, 200, 'primary can see the view');
 
         // Check Category 3 is open and Tag id 27 is selected
-        t.ok(res.payload.indexOf('id="3" checked="checked"') > -1, 'The third category is open');
+        t.ok(res.payload.indexOf('id="cat_3" checked="checked"') > -1, 'The third category is open');
         t.ok(res.payload.indexOf('value="27" checked="checked">') > -1, 'The correct tag is already selected');
 
         // Check Category 1 is open and Tag id 1 is selected
-        t.ok(res.payload.indexOf('id="1" checked="checked"') > -1, 'The first category is open');
+        t.ok(res.payload.indexOf('id="cat_1" checked="checked"') > -1, 'The first category is open');
         t.ok(res.payload.indexOf('value="1" checked="checked">') > -1, 'The first tag is already selected');
 
         // To (ever so slightly) improve the validity of these tests,
         // check that category 2 is not open and tag 2 is not checked
-        t.equal(res.payload.indexOf('id="2" checked="checked"'), -1, 'The second category is not open');
+        t.equal(res.payload.indexOf('id="cat_2" checked="checked"'), -1, 'The second category is not open');
         t.equal(res.payload.indexOf('value="2" checked="checked">'), -1, 'The second tag is not selected');
 
         t.end();
