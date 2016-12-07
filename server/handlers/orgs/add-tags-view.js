4281fea27f4490528f7bfa79d5c22d9098564daf
Jack Murphy <jmurphy.web@gmail.com>
2016-12-07 09:45:19 +0000

    #426 make permissions test pass

--- server/handlers/orgs/add-tags-view.js
+++ server/handlers/orgs/add-tags-view.js
@@ -3,17 +3,18 @@ var Boom = require('boom');
 var helpers = require('../helpers.js');
 
 module.exports = function (request, reply) {
-  var orgId = request.params.id;
+  var orgId = parseInt(request.params.id, 10);
   var loggedIn = request.auth.credentials;
-  var permissions = helpers.getPermissions(loggedIn, 'organisation_id', orgId);
+
   if (loggedIn.organisation_id !== orgId && loggedIn.scope !== 'admin') {
     return reply(Boom.unauthorized('You do not have permission to edit that organisation.'));
   }
 
 
   request.server.methods.pg.tags.getTagsForEdit('organisations', orgId, function (pgErr, tags) {
-    Hoek.assert(!pgErr, 'Database Error');
+    var permissions = helpers.getPermissions(loggedIn, 'organisation_id', orgId);
     var options = Object.assign({tags}, permissions);
+    Hoek.assert(!pgErr, 'Database Error');
 
     return reply.view('tags', options);
 
