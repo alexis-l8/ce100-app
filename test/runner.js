ec98c8a9ac7158adde283584c6e8c61826dc1103
Jack Murphy <jmurphy.web@gmail.com>
2016-12-07 17:27:43 +0000

    #430 adds tests for add tags to org handler

--- test/runner.js
+++ test/runner.js
@@ -1,8 +1,9 @@
 'use strict';
 
 require('./organisations/add-tags-to-org.test.js');
+require('./organisations/add-tags-to-org-view.test.js');
 require('./organisations/toggle-active.test.js');
-require('./organisations/add-org.test.js')
+require('./organisations/add-org.test.js');
 require('./organisations/browse-orgs.test.js');
 require('./organisations/org-details-view.test.js');
 require('./organisations/edit-org-view.test.js');
