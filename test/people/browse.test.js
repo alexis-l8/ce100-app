a0b961acb9f3d940259c38799df5b59df74a7aaf
mk4111 <marie.kasai@live.co.uk>
2016-12-07 16:47:34 +0000

    updates tests

--- test/people/browse.test.js
+++ test/people/browse.test.js
@@ -7,6 +7,13 @@ var config = require('../../server/config.js');
 
 var adminToken = sessions.tokens(config.jwt_secret).admin_1;
 var primaryToken = sessions.tokens(config.jwt_secret).primary_3;
+var users = require('../../data/people.json');
+
+function activeOnly () {
+  return users.filter(function (userObj) {
+    return userObj.active === true;
+  });
+}
 
 var browseAll = function (cookie) {
   return {
@@ -35,36 +42,6 @@ tape('/people endpoint unsuccessful when not logged in',
     });
   });
 
-// /people route accessible by admin
-tape('access /people as a logged-in admin', function (t) {
-  sessions.addAll(function () {
-    init(config, function (error, server, pool) {
-      t.ok(!error, 'no initialising error');
-      server.inject(browseAll(adminToken), function (res) {
-        t.equal(res.statusCode, 200, 'route accessible to admin');
-        t.end();
-        server.stop();
-        pool.end();
-      });
-    });
-  });
-});
-
-// /people route accessible by primary
-tape('access /people as a logged-in primary user', function (t) {
-  sessions.addAll(function () {
-    init(config, function (error, server, pool) {
-      t.ok(!error, 'no initialising error');
-      server.inject(browseAll(primaryToken), function (res) {
-        t.equal(res.statusCode, 200, 'route accessible to admin');
-        t.end();
-        server.stop();
-        pool.end();
-      });
-    });
-  });
-});
-
 // /people route displays all active and inactive users to admins
 tape('check all active + inactive users displayed', function (t) {
   var userIdentifier = '<span class="list__data list__data--primary">';
@@ -74,7 +51,8 @@ tape('check all active + inactive users displayed', function (t) {
     init(config, function (error, server, pool) {
       t.ok(!error, 'no initialising error');
       server.inject(browseAll(adminToken), function (res) {
-        t.equal(res.payload.match(regex).length, 11, 'all users displayed');
+        t.equal(res.statusCode, 200, 'route accessible to admin');
+        t.equal(res.payload.match(regex).length, users.length, 'all users displayed');
         t.end();
         server.stop();
         pool.end();
@@ -83,7 +61,7 @@ tape('check all active + inactive users displayed', function (t) {
   });
 });
 
-// /people route displays all active and inactive users to admins
+// /people route displays all active users to primary
 tape('check only active users displayed', function (t) {
   var userIdentifier = '<span class="list__data list__data--primary">';
   var regex = new RegExp(userIdentifier, 'g');
@@ -92,30 +70,8 @@ tape('check only active users displayed', function (t) {
     init(config, function (error, server, pool) {
       t.ok(!error, 'no initialising error');
       server.inject(browseAll(primaryToken), function (res) {
-        t.equal(res.payload.match(regex).length, 9, 'active users displayed');
-        t.end();
-        server.stop();
-        pool.end();
-      });
-    });
-  });
-});
-
-// /people route displays users in alphabetical order (admin)
-tape('check users in alphabetical order', function (t) {
-  sessions.addAll(function () {
-    init(config, function (error, server, pool) {
-      t.ok(!error, 'no initialising error');
-      server.inject(browseAll(primaryToken), function (res) {
-        t.ok(res.result.indexOf('Ben Matthews')
-          > res.result.indexOf('Alex Wijns'),
-          'Alex followed by Ben (first name)');
-        t.ok(res.result.indexOf('Gale Simon')
-          > res.result.indexOf('Ben Matthews'),
-          'Ben followed by Gale (first name)');
-        t.ok(res.result.indexOf('Sally Robbins')
-          > res.result.indexOf('Gale Simon'),
-          'Gale followed by Sally (first name)');
+        t.equal(res.statusCode, 200, 'route accessible to primary');
+        t.equal(res.payload.match(regex).length, activeOnly().length, 'active users displayed');
         t.end();
         server.stop();
         pool.end();
