5cbb514bd33b2d8ad2e194955a0bd8fb131d8154
SimonLab <simon.labondance@gmail.com>
2017-01-09 16:13:42 +0000

    clean console.log #55

--- public/js/index.js
+++ public/js/index.js
@@ -2,26 +2,18 @@ var logoSaver = (function () {
   var file_name;
 
   var saveFile = function (addedFile) {
-    console.log('saving file,', addedFile);
     file_name = addedFile[0].name;
   };
-
-  function submitForm (id) {
-    console.log('submittting , ', file_name, id);
-    sendFileToServer(file_name, id);
-  }
 
   function sendFileToServer (file_name, id) {
     var editOrgForm = document.getElementById('edit-org-form');
     var formData = new FormData(editOrgForm);
-    console.log(formData);
-    file_name && formData.append('file_name', file_name);
     var xhttp = new XMLHttpRequest();
+
+    file_name && formData.append('file_name', file_name);
     xhttp.onreadystatechange = function () {
-      console.log(this);
       if (this.readyState === 4) {
-        console.log(this);
-        if(this.status === 200) {
+        if (this.status === 200) {
           window.location.replace('/orgs/' + id + '/tags');
         } else {
           document.write(xhttp.responseText);
@@ -29,8 +21,11 @@ var logoSaver = (function () {
       }
     };
     xhttp.open('POST', '/orgs/' + id + '/edit', true);
-    console.log(id, formData);
     xhttp.send(formData);
+  }
+
+  function submitForm (id) {
+    sendFileToServer(file_name, id);
   }
 
   return {
