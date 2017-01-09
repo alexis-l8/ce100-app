5e5c566b8308e25469059a5a65177123c718afbe
Jack Murphy <jmurphy.web@gmail.com>
2017-01-09 17:04:21 +0000

    #487 remove comments

--- server/good-console-options.js
+++ server/good-console-options.js
@@ -1,14 +1,11 @@
 'use strict'
 
 module.exports = {
-  // ops: {
-  //   interval: 5 * 60 * 1000 // reporting interval (5 minutes)
-  // },
   reporters: {
     myConsoleReporter: [{
       module: 'good-squeeze',
       name: 'Squeeze',
-      args: [{ log: '*', error: '*', response: '*', request: '*' /* , ops: '*' */ }]
+      args: [{ log: '*', error: '*', response: '*', request: '*' }]
     }, {
       module: 'good-console'
     }, 'stdout']
