ae75131ea0039c5e713f2ea8f87fd746624b02f9
Jack Murphy <jmurphy.web@gmail.com>
2017-01-15 19:20:09 +0000

    #540 stub send email function with sinon

--- test/people/add-user.test.js
+++ test/people/add-user.test.js
@@ -4,7 +4,8 @@ var tape = require('tape');
 var sessions = require('../helpers/add-sessions.js');
 var init = require('../../server/server.js');
 var config = require('../../server/config.js');
-
+var sinon = require('sinon');
+var sendEmail = require('../../server/email.js');
 
 var addUser = function (userObj) {
   return {
@@ -43,32 +44,36 @@ tape('orgs/add failing validation test', function (t) {
   });
 });
 
-// comment test out until we implement nock
-// tape('orgs/add add user with no organisation', function (t) {
-//   sessions.addAll(function () {
-//     init(config, function (error, server, pool) {
-//
-//       var userObj = {
-//         first_name: 'Jaja',
-//         last_name: 'Bink',
-//         email: 'jaja@gmail.com',
-//         phone: '+44208837733',
-//         user_type: 'primary',
-//         job_title: 'CEO',
-//         org_id: -1
-//       };
-//
-//       server.inject(addUser(userObj), function (res) {
-//         var expected = { org_id: null, id: 12, org_name: null };
-//
-//         t.deepEqual(res.result, expected, 'successful email returns with new user details');
-//         t.equal(res.statusCode, 302, 'admin is redirected after successful new user');
-//
-//         t.end();
-//         server.stop();
-//         pool.end();
-//
-//       });
-//     });
-//   });
-// });
+tape('orgs/add add user with no organisation', function (t) {
+  sessions.addAll(function () {
+    init(config, function (error, server, pool) {
+
+      var userObj = {
+        first_name: 'Jaja',
+        last_name: 'Bink',
+        email: 'jmurphy.web@gmail.com',
+        phone: '+44208837733',
+        user_type: 'primary',
+        job_title: 'CEO',
+        org_id: -1
+      };
+
+      // sinon will call the function we pass as the 3rd argument instead of sendEmail.newUser function.
+      // it will callback with no error.
+      var emailSender = sinon.stub(sendEmail, 'newUser', function ({}, cb) {
+        cb(null); // We only check that there is no error in the handler
+      });
+      server.inject(addUser(userObj), function (res) {
+        var expected = { org_id: null, id: 12, org_name: null };
+
+        emailSender.restore(); // restore the send emails normal functionality
+        t.deepEqual(res.result, expected, 'successful email returns with new user details');
+        t.equal(res.statusCode, 302, 'admin is redirected after successful new user');
+
+        t.end();
+        server.stop();
+        pool.end();
+      });
+    });
+  });
+});
