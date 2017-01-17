4fa6e1d1757b05abc2616130df61ec82d15a723d
mk4111 <marie.kasai@live.co.uk>
2017-01-17 21:14:34 +0000

    updates models/permissions

--- server/models/admin-add-user.js
+++ server/models/admin-add-user.js
@@ -8,7 +8,7 @@ module.exports = {
     org_id: Joi.number().min(-1),
     job_title: Joi.string().allow(''),
     phone: Joi.string().regex(/[0-9]+/).min(11).allow(''),
-    user_type: Joi.string().valid('admin', 'primary')
+    user_type: Joi.string().valid('admin', 'primary', 'secondary')
   },
   failAction: require('../handlers/people/add-view.js')
 };
--- server/models/edit-user.js
+++ server/models/edit-user.js
@@ -18,7 +18,7 @@ var schema = {
     email: Joi.string().email().required(),
     phone: Joi.string().regex(/[0-9]+/).min(11).allow(''),
     org_id: Joi.number().min(-1).required(),
-    user_type: Joi.string().valid('admin', 'primary').required()
+    user_type: Joi.string().valid('admin', 'primary', 'secondary').required()
   },
   primary: {
     first_name: Joi.string().min(1).required(),
