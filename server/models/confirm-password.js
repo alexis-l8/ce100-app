dbefbbf192a4a0d965ea8662d02eae7ee1759fc3
Jack Murphy <jmurphy.web@gmail.com>
2017-01-20 17:58:45 +0000

    #7 add dynamic confirm password validation model

--- server/models/confirm-password.js
+++ server/models/confirm-password.js
@@ -1,9 +1,18 @@
 var Joi = require('joi');
+var activateAccountView = require('../handlers/people/activate-account-view.js');
 
 module.exports = {
   payload: {
     password: Joi.string().min(6).required(),
     confirm_password: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' } } })
   },
-  failAction: require('../handlers/people/activate-account-view.js')
+  failAction: function (req, rep, src, err) {
+    // we want the user to be redirected to the correct version of activate
+    // account view
+    var userFlow = req.path.indexOf('password-reset') > -1
+      ? 'reset'
+      : 'activate';
+      
+    return activateAccountView(userFlow)(req, rep, src, err);
+  }
 };
