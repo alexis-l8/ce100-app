d0ebefa269656b4c089e97b463ca0f297c17af39
Jack Murphy <jmurphy.web@gmail.com>
2017-01-23 12:10:30 +0000

    #7 Add tests to the request new password GET and POST routes

--- server/handlers/auth/password-reset.js
+++ server/handlers/auth/password-reset.js
@@ -5,12 +5,13 @@ var sendEmail = require('../../email.js');
 var Hoek = require('hoek');
 
 module.exports = function (request, reply) {
-  // var userEmail = request.payload.email;
-  request.server.methods.pg.people.getBy('email', 'ce100.emf@gmail.com', function (pgErr, pgRes) {
+  var userEmail = request.payload.email;
+  request.server.methods.pg.people.getBy('email', userEmail, function (pgErr, pgRes) {
     Hoek.assert(!pgErr, 'database error');
     // if no email address found, let the user know
     if (pgRes.length === 0) {
-      return reply.view('request-password-reset', { error: { message: 'Email not found' } });
+      return reply.view('request-password-reset',
+        { error: { message: 'Email not found' } }).code(400);
     }
 
 
