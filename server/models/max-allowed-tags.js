7883ccc2f10fe41ed949a4cbc20908a13c7d4b7e
Jack Murphy <jmurphy.web@gmail.com>
2017-01-26 17:42:09 +0000

    #391 remove unused branch in validation for maximum tags

--- server/models/max-allowed-tags.js
+++ server/models/max-allowed-tags.js
@@ -6,7 +6,6 @@ var failOrg = require('../handlers/orgs/add-tags-view.js');
 var failInsight = require('../handlers/insights/add-tags-view.js');
 
 var schema = {
-  empty: Joi.any().valid(''),
   single: Joi.string(),
   multiple: Joi.array().max(10)
     .options({
@@ -19,15 +18,7 @@ var schema = {
 
 module.exports = {
   payload: function (value, options, next) {
-    var tags;
-
-    if (typeof value.tags === 'string') {
-      tags = value.tags === ''
-      ? schema.empty
-      : schema.single;
-    } else {
-      tags = schema.multiple;
-    }
+    var tags = typeof value.tags === 'string' ? schema.single : schema.multiple;
 
     return Joi.validate(value, { tags: tags }, next);
   },
