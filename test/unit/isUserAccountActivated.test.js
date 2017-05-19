21a0f9cb41b4964f9b744666b50f978adcbd191f
sohilpandya <sohilpandya@me.com>
2017-05-19 17:01:26 +0100

    updated comments in the test as mentioned by @simonlab related to #840

--- test/unit/isUserAccountActivated.test.js
+++ test/unit/isUserAccountActivated.test.js
@@ -3,14 +3,14 @@
 var tape = require('tape');
 var isUserAccountActivated = require('../../templates/helpers/isUserAccountActivated.js');
 
-// first array builds dummy request, second string is expected outcome, third is test comment
+// array with the list of 3 outcomes from running the  function.
 var tests = [
   [[true, true, "active"], true, 'user has an active account'],
   [[false, null, "inactive"], true, 'user account is inactive'],
   [[true, false, "pending"], true, 'user account is pending']
 ];
 
-// cancel url function
+// testing each outcome and its results
 tape('check what the account status is for a user', function (t) {
   tests.forEach(function (test) {
     var actual = isUserAccountActivated(test[0][0], test[0][1], test[0][2])
