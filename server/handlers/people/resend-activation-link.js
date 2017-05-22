bfe36c7bc9de187e7a9f680754b7448a4affc0a7
simonLab <simon.labondance@gmail.com>
2017-05-22 13:04:10 +0100

    change peopleId to userId, https://github.com/emfoundation/ce100-app/pull/847#discussion_r117722774

--- server/handlers/people/resend-activation-link.js
+++ server/handlers/people/resend-activation-link.js
@@ -6,9 +6,9 @@ var sendEmail = require('../../email.js');
 
 module.exports = function (request, reply) {
 
-  var peopleId = request.params.id && parseInt(request.params.id);
+  var userId = request.params.id && parseInt(request.params.id);
 
-  request.server.methods.pg.people.getBy('id', peopleId, function (pgErr, people) {
+  request.server.methods.pg.people.getBy('id', userId, function (pgErr, people) {
     Hoek.assert(!pgErr, 'database error');
     var user = people[0];
     sendEmail('welcome', user, function (emailErr) {
