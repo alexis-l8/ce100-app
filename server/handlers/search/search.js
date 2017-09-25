b50e20eedcd9336c0db7230dee16a19cef526917
simonLab <simon.labondance@gmail.com>
2017-09-25 17:36:47 +0100

    transform search term to lower case, #910

--- server/handlers/search/search.js
+++ server/handlers/search/search.js
@@ -5,7 +5,7 @@ var Hoek = require('hoek');
 module.exports = function (request, reply) {
   var term = request.query.term;
 
-  request.server.methods.pg.organisations.orgsSearch(term,
+  request.server.methods.pg.organisations.orgsSearch(term.toLowerCase(),
     function (pgErr, listOfOrgs) {
       Hoek.assert(!pgErr, 'error getting matching orgs');
       return reply.view('search/search_results', {orgs: listOfOrgs});
