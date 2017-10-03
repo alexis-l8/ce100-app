d6c9e74199adab1e378fc78ad387a0a7745d7a6d
simonLab <simon.labondance@gmail.com>
2017-10-03 14:54:18 +0100

    get all data for a search, implemented handler backend done, #910

--- server/handlers/search/search.js
+++ server/handlers/search/search.js
@@ -3,11 +3,39 @@
 var Hoek = require('hoek');
 
 module.exports = function (request, reply) {
-  var term = request.query.term;
+  var term = request.query.term.toLowerCase();
+  search(request, term, function (result) {
+    return reply.view('search/search_results', result);
+  });
+};
 
-  request.server.methods.pg.organisations.orgsSearch(term.toLowerCase(),
-    function (pgErr, listOfOrgs) {
-      Hoek.assert(!pgErr, 'error getting matching orgs');
-      return reply.view('search/search_results', {orgs: listOfOrgs});
+function search(request, term, cb) {
+  var pg = request.server.methods.pg;
+  var result = {};
+
+  pg.people.peopleSearch(term,
+    function (peopleError, people) {
+      Hoek.assert(!peopleError, 'error people search');
+      result.people = people;
+
+      pg.organisations.orgsSearch(term,
+        function (orgsError, orgs) {
+          Hoek.assert(!orgsError, 'error organisations search');
+          result.orgs = orgs
+
+          pg.challenges.challengesSearch(term,
+            function (challengesErr, challenges) {
+              Hoek.assert(!challengesErr, 'error challenges search');
+              result.challenges = challenges;
+
+              pg.insights.insightsSearch(term,
+                function (insightsErr, insights) {
+                  Hoek.assert(!insightsErr, 'error insights search');
+                  result.insights = insights;
+
+                  return cb(result);
+                });
+            });
+        });
     });
 };
