6b733dbde44088fc8a30c609e74bde01098b9767
simonLab <simon.labondance@gmail.com>
2017-10-09 11:11:34 +0100

    display data on view, #886

--- server/handlers/orgs/details-view.js
+++ server/handlers/orgs/details-view.js
@@ -11,10 +11,10 @@ module.exports = function (request, reply) {
 
   // get organisation main details
   request.server.methods.pg.organisations.orgsGetById(orgId, function (error, orgData) {
-    result.org = orgData;
+    result.org = orgData[0];
     // get tags linked to the organisation
     request.server.methods.pg.organisations.orgsGetTags(orgId, function (error, tags) {
-      result.tags = tags;
+      result.org.tags = tags;
 
       // get challenges of the organisation
       request.server.methods.pg.challenges.getByOrgId(orgId, function (error, challenges) {
@@ -24,38 +24,26 @@ module.exports = function (request, reply) {
         request.server.methods.pg.people.peopleGetByOrgId(orgId, function (error, people) {
           result.people = people;
 
-          return reply(JSON.stringify(result, null, 2));
+          var blockAccess =
+            !(loggedIn.scope === 'admin' || loggedIn.scope === 'content-owner')
+            && !result.org.org_active;
+          // If the organisation is not active, then only an admin and content-owner can view
+          if (blockAccess) {
+            return reply(Boom.forbidden('You cannot access that organisation'));
+          }
+
+          var options = Object.assign(
+            {},
+            result,
+            { view: helpers.getView(request.path) },
+            {topNavBarType: topNavBarType},
+            permissions
+          );
+
+          return reply.view('organisations/details', options);
         })
 
       })
     });
   });
-
-
-  // request.server.methods.pg.organisations.getDetails(orgId, function (error, orgData) {
-  //   console.log(orgData);
-  //
-  //   // organisation (id, name, mission_statement, logo_url, active)
-  //     // tags (id, name)
-  //   // challenges (id, title)
-  //   //people (first_nane, last_name, user_type, logo_url, email, phone, job_title, active)
-  //   Hoek.assert(!error, 'Error retrieving organisation with id ' + orgId)
-  //   var blockAccess =
-  //     !(loggedIn.scope === 'admin' || loggedIn.scope === 'content-owner')
-  //     && !orgData.org.active;
-  //   // If the organisation is not active, then only an admin and content-owner can view
-  //   if (blockAccess) {
-  //     return reply(Boom.forbidden('You cannot access that organisation'));
-  //   }
-  //
-  //   var options = Object.assign(
-  //     {},
-  //     orgData,
-  //     { view: helpers.getView(request.path) },
-  //     {topNavBarType: topNavBarType},
-  //     permissions
-  //   );
-  //
-  //   return reply.view('organisations/details', options);
-  // });
 };
