a85f00c58743f68b9ea7dc15351b5fd3ccdc0d14
simonLab <simon.labondance@gmail.com>
2017-10-11 14:29:59 +0100

    update test admin role with challenges, #888

--- test/challenges/toggle-active.test.js
+++ test/challenges/toggle-active.test.js
@@ -25,13 +25,13 @@ function toggleActivity (token) {
   };
 }
 
-tape('Admin cannot unarchive/archive a challenge - ' + __filename, function (t) {
+tape('Admin can unarchive/archive a challenge - ' + __filename, function (t) {
   sessions.addAll(function () {
     init(config, function (error, server, pool) {
       if (error) { console.log(error); }
 
       server.inject(toggleActivity(admin), function (res) {
-        t.equal(res.statusCode, 403, 'admin does not have permission to unarchive/archive challenge');
+        t.equal(res.statusCode, 302, 'admin does not have permission to unarchive/archive challenge');
         t.end();
         pool.end();
         server.stop();
